90c94a2aae92dee7329450ce9ca8b20a6e4b8de3
Nicola Ferralis <feranick@hotmail.com>
2014-10-17 22:38:14 -0400

    Optimized DAC parameters.

--- solarlogmeter2/solarlogmeter2.ino
+++ solarlogmeter2/solarlogmeter2.ino
@@ -3,7 +3,7 @@
  
  SolarLogMeter (with weather measurements)						 
  		
- v. 2.0.1 - PV IV logging 
+ v. 2.0.2 - PV IV logging 
  2011-2014 - Nicola Ferralis - ferralis@mit.edu		
  
  With contribution from IVy: 
@@ -161,7 +161,7 @@
 //------------------
 
 String nameProg = "SolarLogMeter";
-String versProg = "2.0.1 - 20141008";
+String versProg = "2.0.2 - 20141017";
 String developer = "Nicola Ferralis - ferralis@mit.edu";
 char cfgFile[]="SLM.cfg";
 
@@ -235,7 +235,7 @@ float precIoc = 0.1;  // minimum current (mA) to determine Voc
 unsigned long restTime = 12;  //Time in between IV scans (minutes)
 unsigned int delayTime = 10; // Generic time delay (ms). Fallback in case no SD card is found.
 
-float refV = 0.312;
+float refV = 0.305;
 float startV = 0.0;
 float stopV = 4.096;  //This is set by default as a fallback in case no SD card is found.
 
@@ -2315,6 +2315,7 @@ void resetVOpAmp() {
  
   // for lower current threshold:
   dacWrite(B10010001, B00101100);
+  //dacWrite(B10010000, B00011001);
   
   //set op amp negative terminal voltage to 1V 
   //int vOpAmp = dacWrite(B10010011, B11101000);
